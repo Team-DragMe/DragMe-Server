c79c14bfdcfa1956300b5e279cf04bddf6a41538
Minjae Kang <rkdalswo1021@naver.com>
2022-07-12 13:09:28 +0900

    [FEAT] MongoDB Collection 명시적으로 생성

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import config from '../config';
 import User from '../models/User';
+import Information from '../models/Information';
 
 const connectDB = async () => {
   try {
@@ -12,6 +13,10 @@ const connectDB = async () => {
 
     User.createCollection().then(function (collection) {
       console.log('User Collection is created!');
+    });
+
+    Information.createCollection().then(function (collection) {
+      console.log('Information Collection is created!');
     });
   } catch (err: any) {
     console.error(err.message);
