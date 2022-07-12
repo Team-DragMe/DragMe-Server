989fac379e87ae9c80ee790de7b7fff232dbf0bc
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-12 12:41:22 +0900

    [FEAT] MongoDB Collection 명시적으로 생성

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,17 +1,22 @@
-import mongoose from "mongoose";
-import config from "../config";
+import mongoose from 'mongoose';
+import config from '../config';
+import User from '../models/User';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(config.mongoURI);
+  try {
+    await mongoose.connect(config.mongoURI);
 
-        mongoose.set('autoCreate', true);
+    mongoose.set('autoCreate', true);
 
-        console.log("Mongoose Connected ...");
-    } catch (err: any) {
-        console.error(err.message);
-        process.exit(1);
-    }
+    console.log('Mongoose Connected ...');
+
+    User.createCollection().then(function (collection) {
+      console.log('User Collection is created!');
+    });
+  } catch (err: any) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
 
 export default connectDB;
