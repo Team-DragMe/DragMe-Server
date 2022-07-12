4d70e3898a6e6cff24f4ae5e8286c28ec5ceddc0
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-12 16:16:00 +0900

    [FIX] Information 컬렉션에 timestamps 추가

--- src/models/Information.ts
+++ src/models/Information.ts
@@ -1,19 +1,24 @@
 import mongoose from 'mongoose';
 import { InformationInfo } from '../interfaces/information/InformationInfo';
 
-const InformationSchema = new mongoose.Schema({
-  date: {
-    type: String,
-    required: true,
+const InformationSchema = new mongoose.Schema(
+  {
+    date: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+    },
   },
-  type: {
-    type: String,
-    required: true,
-  },
-  value: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model<InformationInfo & mongoose.Document>(
   'Information',
