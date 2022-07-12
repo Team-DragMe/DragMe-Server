d7a9c997b97cd3aa1ff3748babeba94f40f65842
Minjae Kang <rkdalswo1021@naver.com>
2022-07-13 00:48:47 +0900

    [FEAT] Information Router 연결

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 //router index file
 import { Router } from 'express';
-
+import InformationRouter from './InformationRouter';
 
 const router = Router();
 
-
+router.use('/information', InformationRouter);
 
 export default router;
