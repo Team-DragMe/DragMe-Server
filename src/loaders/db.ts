55500ae2896b3c1ab04ee2710e5f2f97e84f6550
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-12 15:57:11 +0900

    [FEAT] Schedule MongoDB Collection 명시적으로 생성

--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import config from '../config';
 import User from '../models/User';
 import Information from '../models/Information';
+import Schedule from '../models/Schedule';
 
 const connectDB = async () => {
   try {
@@ -17,6 +18,10 @@ const connectDB = async () => {
 
     Information.createCollection().then(function (collection) {
       console.log('Information Collection is created!');
+    });
+
+    Schedule.createCollection().then(function (collection) {
+      console.log('Schedule Collection is created!');
     });
   } catch (err: any) {
     console.error(err.message);
