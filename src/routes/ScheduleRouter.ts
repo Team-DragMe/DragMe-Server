297353c846095405f167ee36d351a132d6e5fd13
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-13 16:10:25 +0900

    [FEAT] 일간 계획 계획블록 리스트 조회 Router - Controller 연결

--- src/routes/ScheduleRouter.ts
+++ src/routes/ScheduleRouter.ts
@@ -5,4 +5,6 @@ const router: Router = Router();
 
 router.post('/', ScheduleController.createSchedule);
 router.patch('/day-reschedule', ScheduleController.dayReschedule);
+router.get('/days', ScheduleController.getDailySchedules);
+
 export default router;
