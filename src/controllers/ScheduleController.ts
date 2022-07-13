9c680afd8027cc155c67fcceeda9640dcf4bba59
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-13 23:19:28 +0900

    [FEAT] 계획블록 이름 수정 Controller 구현

--- src/controllers/ScheduleController.ts
+++ src/controllers/ScheduleController.ts
@@ -5,6 +5,7 @@ import statusCode from '../modules/statusCode';
 import { ScheduleCreateDto } from '../interfaces/schedule/ScheduleCreateDto';
 import ScheduleService from '../services/ScheduleService';
 import mongoose from 'mongoose';
+import { ScheduleUpdateDto } from '../interfaces/schedule/ScheduleUpdateDto';
 
 /**
  * @route POST /schedule
@@ -113,8 +114,47 @@ const getDailySchedules = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * @route PATCH /schedule/title
+ * @desc Update Schedule Title
+ * @access Public
+ */
+const updateScheduleTitle = async (req: Request, res: Response) => {
+  // let { scheduleId } = req.body; // 이후에 scheduleId까지 받기
+  const { newTitle } = req.body;
+  const scheduleId = new mongoose.Types.ObjectId('62cd876a77bc33d906978333');
+
+  if (!scheduleId || !newTitle) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+  }
+
+  const scheduleUpdateDto: ScheduleUpdateDto = {
+    title: newTitle,
+  };
+
+  try {
+    await ScheduleService.updateScheduleTitle(scheduleId, scheduleUpdateDto);
+    res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, message.UPDATE_SCHEDULE_TITLE_SUCCESS));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          message.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
+};
+
 export default {
   createSchedule,
   dayReschedule,
   getDailySchedules,
+  updateScheduleTitle,
 };
