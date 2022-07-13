1467627c065d6a2e184246cace87d09c17918cfc
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-13 23:17:18 +0900

    [FEAT] 계획블록 이름 수정 Router - Controller 연결

--- src/routes/ScheduleRouter.ts
+++ src/routes/ScheduleRouter.ts
@@ -6,5 +6,6 @@ const router: Router = Router();
 router.post('/', ScheduleController.createSchedule);
 router.patch('/day-reschedule', ScheduleController.dayReschedule);
 router.get('/days', ScheduleController.getDailySchedules);
+router.patch('/title', ScheduleController.updateScheduleTitle);
 
 export default router;
