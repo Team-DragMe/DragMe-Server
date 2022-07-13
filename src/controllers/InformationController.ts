54719d3689475f20a071f1764f199f11f5a77bcc
Minjae Kang <rkdalswo1021@naver.com>
2022-07-13 18:27:43 +0900

    [FEAT] InformationController 구현

--- src/controllers/InformationController.ts
+++ src/controllers/InformationController.ts
@@ -1,6 +1,6 @@
 import express, { Request, response, Response } from 'express';
 import statusCode from '../modules/statusCode';
-import responseMessage from '../modules/responseMessage';
+import message from '../modules/responseMessage';
 import util from '../modules/util';
 import { DailyMemoCreateDto } from '../interfaces/information/DailyMemoCreateDto';
 import InformationService from '../services/InformationService';
@@ -17,9 +17,7 @@ const createDailyMemo = async (req: Request, res: Response) => {
 
     res
       .status(statusCode.CREATED)
-      .send(
-        util.success(statusCode.CREATED, responseMessage.CREATE_MEMO_SUCCESS)
-      );
+      .send(util.success(statusCode.CREATED, message.CREATE_MEMO_SUCCESS));
   } catch (error) {
     console.log(error);
     res
@@ -27,10 +25,49 @@ const createDailyMemo = async (req: Request, res: Response) => {
       .send(
         util.fail(
           statusCode.INTERNAL_SERVER_ERROR,
-          responseMessage.INTERNAL_SERVER_ERROR
+          message.INTERNAL_SERVER_ERROR
         )
       );
   }
 };
 
-export default { createDailyMemo };
+/**
+ * @route GET /information/days?date
+ * @desc GET Schedule Information
+ * @access Public
+ */
+const getDailyInformation = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { date } = req.query;
+  try {
+    if (!date) {
+      res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+    } else {
+      const data = await InformationService.getDailyInformation(date as string);
+      res
+        .status(statusCode.OK)
+        .send(
+          util.success(
+            statusCode.OK,
+            message.GET_DAILYSCHEDULE_INFORMATION_SUCESS,
+            data
+          )
+        );
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          message.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
+};
+export default { createDailyMemo, getDailyInformation };
