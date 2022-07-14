b85d7c48ff45f26adec17ba35721c66f24ac221f
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-14 15:38:04 +0900

    [FEAT] 자주 사용하는 계획 등록 Controller 구현

--- src/controllers/ScheduleController.ts
+++ src/controllers/ScheduleController.ts
@@ -6,7 +6,6 @@ import { ScheduleCreateDto } from '../interfaces/schedule/ScheduleCreateDto';
 import ScheduleService from '../services/ScheduleService';
 import mongoose from 'mongoose';
 import { ScheduleUpdateDto } from '../interfaces/schedule/ScheduleUpdateDto';
-import Schedule from '../models/Schedule';
 
 /**
  * @route POST /schedule
@@ -188,10 +187,54 @@ const updateScheduleTitle = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * @route POST /schedule/day-routine
+ * @desc Create Routine with Schedule
+ * @access Public
+ */
+const createRoutine = async (req: Request, res: Response) => {
+  let { scheduleId } = req.body;
+  if (!scheduleId || scheduleId.length != 24) {
+    // 유효하지 않은 scheduleId인 경우 : 400 error
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+  }
+
+  scheduleId = new mongoose.Types.ObjectId(scheduleId);
+  const userId = new mongoose.Types.ObjectId('62cd27ae39f42cfbf520009a');
+
+  try {
+    const newRoutine = await ScheduleService.createRoutine(userId, scheduleId);
+
+    if (!newRoutine) {
+      // newRoutine으로 null이 반환 된 경우(scheduleId에 해당하는 원본 계획블록이 없는 경우) :  404 error
+      return res
+        .status(statusCode.NOT_FOUND)
+        .send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
+    }
+
+    res
+      .status(statusCode.CREATED)
+      .send(util.success(statusCode.CREATED, message.CREATE_ROUTINE_SUCCESS));
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          message.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
+};
+
 export default {
   createSchedule,
   dayReschedule,
   getDailySchedules,
   getReschedules,
   updateScheduleTitle,
+  createRoutine,
 };
