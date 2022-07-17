9795eff4a99fdd6c0bc5c2a719b6a756d9ea886d
Minjae Kang <rkdalswo1021@naver.com>
2022-07-18 05:17:03 +0900

    [FEAT] 계획블록 삭제 Service 구현

--- src/services/ScheduleService.ts
+++ src/services/ScheduleService.ts
@@ -31,6 +31,74 @@ const createSchedule = async (
   }
 };
 
+const deleteSchedule = async (scheduleId: mongoose.Types.ObjectId) => {
+  try {
+    // 삭제할 계획블록 조회
+    const checkDeleteSchedule = await Schedule.findById(scheduleId).populate({
+      path: 'subSchedules',
+      model: 'Schedule',
+    });
+
+    if (!checkDeleteSchedule) {
+      return null;
+    } else {
+      const existingDeleteSchedule = checkDeleteSchedule.subSchedules;
+      if (existingDeleteSchedule.length !== 0) {
+        // 상위 계획이 하위 계획도 있을 경우 하위계획 삭제
+        for (const deleteSubSchedule of checkDeleteSchedule.subSchedules) {
+          await Schedule.findByIdAndDelete(deleteSubSchedule._id);
+        }
+
+        // 상위 계획 삭제
+        await Schedule.findByIdAndDelete(scheduleId);
+      } else {
+        // 상위 계획만 있을 경우 상위 계획만 삭제
+        await Schedule.findByIdAndDelete(scheduleId);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+const completeSchedule = async (
+  scheduleId: mongoose.Types.ObjectId
+): Promise<ScheduleInfo | null> => {
+  try {
+    // scheduleId로 계획블록 조회
+    const checkCompleteSchedule = await Schedule.findById(scheduleId);
+    if (!checkCompleteSchedule) {
+      return null;
+    } else {
+      if (checkCompleteSchedule.isCompleted === true) {
+        await Schedule.findOneAndUpdate(
+          {
+            _id: scheduleId,
+          },
+          {
+            $set: { isCompleted: false, usedTime: [] },
+          },
+          { new: true }
+        );
+      } else {
+        await Schedule.findOneAndUpdate(
+          {
+            _id: scheduleId,
+          },
+          {
+            isCompleted: true,
+          },
+          { new: true }
+        );
+      }
+      return checkCompleteSchedule;
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 const createTime = async (
   scheduleId: mongoose.Types.ObjectId,
   timeDto: TimeDto
@@ -109,43 +177,6 @@ const deleteTime = async (
       }
     }
     return deleteScheduleTime;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-};
-const completeSchedule = async (
-  scheduleId: mongoose.Types.ObjectId
-): Promise<ScheduleInfo | null> => {
-  try {
-    // scheduleId로 계획블록 조회
-    const checkCompleteSchedule = await Schedule.findById(scheduleId);
-    if (!checkCompleteSchedule) {
-      return null;
-    } else {
-      if (checkCompleteSchedule.isCompleted === true) {
-        await Schedule.findOneAndUpdate(
-          {
-            _id: scheduleId,
-          },
-          {
-            $set: { isCompleted: false, usedTime: [] },
-          },
-          { new: true }
-        );
-      } else {
-        await Schedule.findOneAndUpdate(
-          {
-            _id: scheduleId,
-          },
-          {
-            isCompleted: true,
-          },
-          { new: true }
-        );
-      }
-      return checkCompleteSchedule;
-    }
   } catch (error) {
     console.log(error);
     throw error;
@@ -510,6 +541,7 @@ const updateScheduleCategory = async (
 
 export default {
   createSchedule,
+  deleteSchedule,
   createTime,
   deleteTime,
   completeSchedule,
