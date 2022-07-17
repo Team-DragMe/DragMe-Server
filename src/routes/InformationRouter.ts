1f799b1a42f44c557a1bba495410ce193891899d
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-17 23:32:43 +0900

    [FEAT] 일간 목표 작성 Router - Controller 연결

--- src/routes/InformationRouter.ts
+++ src/routes/InformationRouter.ts
@@ -6,5 +6,6 @@ const router: Router = Router();
 router.post('/memo', InformationController.createDailyMemo);
 router.get('/days', InformationController.getDailyInformation);
 router.post('/emoji', InformationController.createEmoji);
+router.post('/days', InformationController.createDailyGoal);
 
 export default router;
