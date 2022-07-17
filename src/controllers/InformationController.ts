092e798cc1c101c3a01e2b1e283b9ed959a5f35a
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-17 23:51:02 +0900

    [CHORE] 정보 생성 Controller로 통일

--- src/controllers/InformationController.ts
+++ src/controllers/InformationController.ts
@@ -6,18 +6,21 @@ import { InformationCreateDto } from '../interfaces/information/InformationCreat
 import InformationService from '../services/InformationService';
 
 /**
- * @route POST /information/memo
- * @desc Create Daily Memo
+ * @route POST /information/
+ * @desc Create Information
  * @access Public
  */
-const createDailyMemo = async (req: Request, res: Response) => {
-  const informationCreateDto: InformationCreateDto = req.body;
+const createInformation = async (req: Request, res: Response) => {
+  let informationCreateDto: InformationCreateDto = req.body;
+  informationCreateDto.userId = '62cd6eb82b6b4e92c7fc08f1';
   try {
-    await InformationService.createDailyMemo(informationCreateDto);
+    await InformationService.createInformation(informationCreateDto);
 
     res
       .status(statusCode.CREATED)
-      .send(util.success(statusCode.CREATED, message.CREATE_MEMO_SUCCESS));
+      .send(
+        util.success(statusCode.CREATED, message.CREATE_INFORMATION_SUCCESS)
+      );
   } catch (error) {
     console.log(error);
     res
@@ -63,66 +66,7 @@ const getDailyInformation = async (req: Request, res: Response) => {
   }
 };
 
-/**
- * @route POST /information/emoji
- * @desc Create Emoji
- * @access Public
- */
-const createEmoji = async (req: Request, res: Response) => {
-  const informationCreateDto: InformationCreateDto = req.body;
-  try {
-    await InformationService.createEmoji(informationCreateDto);
-
-    res
-      .status(statusCode.CREATED)
-      .send(util.success(statusCode.CREATED, message.CREATE_EMOJI_SUCCESS));
-  } catch (error) {
-    console.log(error);
-    res
-      .status(statusCode.INTERNAL_SERVER_ERROR)
-      .send(
-        util.fail(
-          statusCode.INTERNAL_SERVER_ERROR,
-          message.INTERNAL_SERVER_ERROR
-        )
-      );
-  }
-};
-
-/**
- * @route POST /information/days
- * @desc Create Daily Goal
- * @access Public
- */
-const createDailyGoal = async (req: Request, res: Response) => {
-  let informationCreateDto: InformationCreateDto = req.body;
-  informationCreateDto.userId = '62cd6eb82b6b4e92c7fc08f1';
-  informationCreateDto.type = 'dailyGoal';
-
-  try {
-    await InformationService.createDailyGoal(informationCreateDto);
-
-    res
-      .status(statusCode.CREATED)
-      .send(
-        util.success(statusCode.CREATED, message.CREATE_DAILY_GOAL_SUCCESS)
-      );
-  } catch (error) {
-    console.log(error);
-    res
-      .status(statusCode.INTERNAL_SERVER_ERROR)
-      .send(
-        util.fail(
-          statusCode.INTERNAL_SERVER_ERROR,
-          message.INTERNAL_SERVER_ERROR
-        )
-      );
-  }
-};
-
 export default {
-  createDailyMemo,
+  createInformation,
   getDailyInformation,
-  createEmoji,
-  createDailyGoal,
 };
