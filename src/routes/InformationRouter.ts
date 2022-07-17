6c6b9d1c73c727ade44eae55114f8f7a769526ee
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-17 23:40:07 +0900

    [CHORE] 정보 생성 Router로 통일

--- src/routes/InformationRouter.ts
+++ src/routes/InformationRouter.ts
@@ -3,9 +3,7 @@ import { InformationController } from '../controllers';
 
 const router: Router = Router();
 
-router.post('/memo', InformationController.createDailyMemo);
+router.post('/', InformationController.createInformation);
 router.get('/days', InformationController.getDailyInformation);
-router.post('/emoji', InformationController.createEmoji);
-router.post('/days', InformationController.createDailyGoal);
 
 export default router;
