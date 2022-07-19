68d9a22d8ac56e25ddc8a394ea74a1fa004eff59
Minjae Kang <rkdalswo1021@naver.com>
2022-07-19 14:33:41 +0900

    [FEAT] 월간 목표 조회 Information Controller 구현

--- src/controllers/InformationController.ts
+++ src/controllers/InformationController.ts
@@ -66,7 +66,39 @@ const getDailyInformation = async (req: Request, res: Response) => {
   }
 };
 
+/**
+ * @route GET /information/months?date
+ * @desc GET MonthlyGoal Information
+ * @access Public
+ */
+const getMonthlyGoal = async (req: Request, res: Response) => {
+  const { date } = req.query;
+  if (!date) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.NULL_VALUE));
+  }
+  try {
+    const data = await InformationService.getMonthlyGoal(date as string);
+    res
+      .status(statusCode.OK)
+      .send(
+        util.success(statusCode.OK, message.GET_MONTHLY_GOAL_SUCCESS, data)
+      );
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          message.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
+};
 export default {
   createInformation,
   getDailyInformation,
+  getMonthlyGoal,
 };
