c2bca21df4b64503b67d3e3318e668481469a9ed
Hyosik Philip Joo <lux1937@gmail.com>
2022-07-21 14:48:29 +0900

    [FEAT] babel-jest 옵션 추가

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ module.exports = {
   testEnvironment: 'node', // 테스트 환경 'node' 환경을 사용
   verbose: true,
   collectCoverage: true,
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'json'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
 };
