4b9841bb9536dc9effbc8e6f05919316c9a16526
Minjae Kang <rkdalswo1021@naver.com>
2022-07-22 15:39:26 +0900

    [FEAT] 일간 계획 정보 조회 API 테스트 코드

---
+++ test/information.spec.js
@@ -0,0 +1,43 @@
+const request = require('supertest');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+
+const app = require('../src/index');
+
+beforeAll(async () => {
+  console.log('DRAG.ME API TEST START - Route Information');
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log('Mongoose Connected ...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+});
+
+describe('일간 계획 정보 조회 [GET] /information/days?date=', () => {
+    it('200 - 일간 계획 정보 조회 성공', async () => {
+      await request(app)
+        .get('/information/days')
+        .set('Content-Type', 'application/json')
+        .query({ date: '2022-07-22' })
+        .expect(200)
+        .expect('Content-Type', /json/);
+    });
+  
+    it('400 - 필요한 값이 없습니다', async () => {
+      await request(app)
+        .get('/information/days')
+        .set('Content-Type', 'application/json')
+        .expect(400);
+    });
+  });
+
+
+afterAll(async () => {
+    await mongoose.connection.close();
+    console.log('Mongoose Disconnected!');
+    console.log('DRAG.ME API TEST COMPLETE - Route Information');
+  });
